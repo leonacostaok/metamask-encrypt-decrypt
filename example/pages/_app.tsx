1ef50d18db083e358b29a7a3484f2e245408e3a2
Leon Acosta @ Dandelion Labs <leon@dandelionlabs.io>
2023-11-08 16:28:41 +0700

    added google analytics

--- example/pages/_app.tsx
+++ example/pages/_app.tsx
@@ -2,6 +2,7 @@ import { Web3ReactHooks, Web3ReactProvider } from "@web3-react/core";
 import { MetaMask } from "@web3-react/metamask";
 import { AppProps } from "next/app";
 import React, { useEffect } from "react";
+import Script from 'next/script'
 
 import { hooks as metaMaskHooks, metaMask } from "../connectors/metaMask";
 
@@ -30,8 +31,29 @@ export default function App({ Component, pageProps }: AppProps) {
     };
   }, []);
   return (
-    <Web3ReactProvider connectors={connectors}>
-      <Component {...pageProps} />
-    </Web3ReactProvider>
+      <>
+
+        <head>
+          <title>MetaMask Encrypt/Decrypt</title>
+          <meta name="description" content="MetaMask Encrypt and decrypt tools" />
+          <meta property="og:title" content="MetaMask Encrypt/Decrypt" />
+          <meta property="og:description" content="MetaMask Encrypt and decrypt tools" />
+          <meta property="og:url" content="https://metamask-encrypt-decrypt.dandelionlabs.io/" />
+          <meta property="og:type" content="website" />
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-YVFR36G469" />
+          <Script id="google-analytics">
+              {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+   
+            gtag('config', 'G-YVFR36G469');
+          `}
+          </Script>
+        </head>
+        <Web3ReactProvider connectors={connectors}>
+          <Component {...pageProps} />
+        </Web3ReactProvider>
+      </>
   );
 }
