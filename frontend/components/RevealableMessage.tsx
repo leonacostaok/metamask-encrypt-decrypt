1ad03d3f39ddaf2934966dfe797c57d0c97ced68
Leon Acosta @ Dandelion Labs <leon@dandelionlabs.io>
2023-11-27 17:40:14 +0700

    fixed revealed message how it looks

--- frontend/components/RevealableMessage.tsx
+++ frontend/components/RevealableMessage.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { useWeb3React } from '@web3-react/core';
 import { Button } from './Button';
 import styled from 'styled-components';
+import { TextArea } from './TextArea';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 interface RevealableMessageProps {
     encryptedMessage: string
@@ -9,6 +11,7 @@ interface RevealableMessageProps {
 
 export default function RevealableMessage({encryptedMessage}: RevealableMessageProps) {
     const { account, provider } = useWeb3React()
+    const [copied, setCopied] = useState(false)
     const [revealedMessage, setRevealedMessage] = useState<string | null>(null)
 
     const handleRevealMessage = async () => {
@@ -32,11 +35,28 @@ export default function RevealableMessage({encryptedMessage}: RevealableMessageP
         }
     }
 
-    return revealedMessage ? <>{revealedMessage}</> : (
+    return revealedMessage ? (
+        <RevealableMessageWrapper>
+            <TextArea disabled={true}>{revealedMessage}</TextArea>
+            <CopyToClipboard text={encryptedMessage} onCopy={() => {
+                setCopied(true)
+                setTimeout(() => setCopied(false), 3000)
+            }}>
+                <RevealButton disabled={copied}>{copied ? 'Copied!' : 'Copy'}</RevealButton>
+            </CopyToClipboard>
+        </RevealableMessageWrapper>
+    ) : (
         <RevealButton onClick={handleRevealMessage}>Reveal</RevealButton>
     )
 }
 
+const RevealableMessageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  gap: 10px;
+`
 const RevealButton = styled(Button)`
   font-size: 10px;
   padding: 5px 12px;
